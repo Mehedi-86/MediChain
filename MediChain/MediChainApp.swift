df064a2b0e0ae3cbd3ba66d8ee0ccfd62247fb9b
Mehedi-86 <rabby2107086@stud.kuet.ac.bd>
2026-03-03 12:11:36 +0600

    Initial Commit

---
+++ MediChain/MediChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediChainApp.swift
+//  MediChain
+//
+//  Created by mehedi hasan on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MediChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediChainTests/MediChainTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediChainTests.swift
+//  MediChainTests
+//
+//  Created by mehedi hasan on 3/3/26.
+//
+
+import Testing
+@testable import MediChain
+
+struct MediChainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
